59fb5eae681d3682814b58c49dd2c422576b886d
OtexAds Deploy <deploy@otexads.com>
2026-07-25 20:03:42 +0300

    Debug: add button click handler

--- advertiser-portal/src/routes/_app.campaigns.new.tsx
+++ advertiser-portal/src/routes/_app.campaigns.new.tsx
@@ -290,6 +290,7 @@ function NewCampaignPage() {
           </Link>
           <button
             type="submit"
+            onClick={() => console.log("Button clicked!")}
             className="rounded-full bg-primary px-5 py-2.5 text-sm font-medium text-primary-foreground hover:brightness-110"
           >
             Launch campaign
