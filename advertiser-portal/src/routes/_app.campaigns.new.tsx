016e0a75e0e1287c3545f83ed33272cb2ff9d5cd
OtexAds Deploy <deploy@otexads.com>
2026-07-26 07:59:15 +0300

    Debug: add alert to verify button is clickable

--- advertiser-portal/src/routes/_app.campaigns.new.tsx
+++ advertiser-portal/src/routes/_app.campaigns.new.tsx
@@ -290,7 +290,7 @@ function NewCampaignPage() {
           </Link>
           <button
             type="submit"
-            onClick={() => console.log("Button clicked!")}
+            onClick={(e) => { e.preventDefault(); alert("Button is clickable!"); }}
             className="rounded-full bg-primary px-5 py-2.5 text-sm font-medium text-primary-foreground hover:brightness-110"
           >
             Launch campaign
